cb6dec36755e2dae336cd420642a6056e1cf441f
unix-world <iradu@unix-world.org>
2022-03-23 21:49:30 +0200

    Stable, svn.579

--- smartgoext.go
+++ smartgoext.go
@@ -1,13 +1,14 @@
 
 // GO Lang :: SmartGo Extra :: Smart.Go.Framework
 // (c) 2020-2022 unix-world.org
-// r.20220314.1554 :: STABLE
+// r.20220323.1640 :: STABLE
 
 package smartgoext
 
 // REQUIRE: go 1.15 or later
 
 import (
+	"os"
 	"runtime"
 	"errors"
 	"log"
@@ -15,9 +16,192 @@ import (
 	"time"
 	"sort"
 
+	"fmt"
+	"strconv"
+	"bytes"
+
+	"net/http"
+	"crypto/subtle"
+	"golang.org/x/net/webdav"
+
 	"github.com/unix-world/smartgoext/quickjs"
 	smart "github.com/unix-world/smartgo"
 )
+
+
+//-----
+
+func WebdavServer(authUser string, authPass string, httpAddr string, httpPort uint16, httpsPort uint16, serveSecure bool, disableUnsecure bool) bool {
+
+	//-- local defines
+
+	const (
+		THE_VERSION = "r.20220323.1440"
+
+		CONN_HOST = "0.0.0.0"
+		CONN_PORT = 13080
+		CONN_TLSPORT = 13443
+
+		ADMIN_USER = "admin"
+		ADMIN_PASSWORD = "pass"
+
+		STORAGE_DIR = "./wdav"
+		DAV_PATH = "/webdav"
+
+		TLS_CERT = "./ssl/cert.crt"
+		TLS_KEY = "./ssl/cert.key"
+	)
+
+	//-- fixes
+
+	if(smart.StrTrimWhitespaces(authUser) == "") || (smart.StrTrimWhitespaces(authPass) == "") {
+		authUser = ADMIN_USER
+		authPass = ADMIN_PASSWORD
+	} //end if
+
+	httpAddr = smart.StrTrimWhitespaces(httpAddr)
+	if(httpAddr == "") {
+		httpAddr = CONN_HOST
+	} //end if
+
+	if(httpPort <= 0 || httpPort > 65535) {
+		httpPort = CONN_PORT
+	} //end if
+
+	if(httpsPort <= 0 || httpsPort > 65535) {
+		httpsPort = CONN_TLSPORT
+	} //end if
+
+	//-- get cwd path
+
+	path, err := os.Getwd()
+	if(err != nil) {
+		log.Println("[ERROR] WebDAV Server: Cannot Get Current Path: ", err)
+		return false
+	} //end if
+
+	//-- test params
+
+	if(disableUnsecure == true && serveSecure != true) {
+		log.Println("[ERROR] WebDAV Server INIT: The both HTTP and HTTPS modes are disabled ... server will exit ...")
+		return false
+	} //end if
+
+	//-- for web
+
+	var serverSignature bytes.Buffer
+	serverSignature.WriteString("WebDAV GO Server " + THE_VERSION + "\n")
+	serverSignature.WriteString("(c) 2020-2022 unix-world.org" + "\n")
+	serverSignature.WriteString("\n")
+	if(disableUnsecure != true) {
+		serverSignature.WriteString("<URL> :: http://" + httpAddr + ":" + smart.ConvertUInt16ToStr(httpPort) + DAV_PATH + "/" + "\n")
+	} //end if
+	if(serveSecure == true) {
+		serverSignature.WriteString("<Secure URL> :: https://" + httpAddr + ":" + smart.ConvertUInt16ToStr(httpsPort) + DAV_PATH + "/" + "\n")
+	} //end if
+
+	//-- for console
+
+	fmt.Println("===========================================================================")
+	fmt.Println("WebDAV GO Server " + THE_VERSION)
+	fmt.Println("---------------------------------------------------------------------------")
+	fmt.Println("Current Path: " + string(path))
+	fmt.Println("DAV Folder: " + STORAGE_DIR)
+	fmt.Println("WebDAV Path: " + DAV_PATH)
+	fmt.Println("---------------------------------------------------------------------------")
+	if disableUnsecure != true {
+		fmt.Println("Unsecure Listening at http://" + httpAddr + ":" + smart.ConvertUInt16ToStr(httpPort) + "/")
+	}
+	if serveSecure == true {
+		fmt.Println("Secure Listening TLS at https://" + httpAddr + ":" + smart.ConvertUInt16ToStr(httpsPort) + "/")
+	}
+	fmt.Println("===========================================================================")
+
+	//--
+
+	srv := &webdav.Handler{
+		Prefix:     DAV_PATH,
+		FileSystem: webdav.Dir(STORAGE_DIR),
+		LockSystem: webdav.NewMemLS(),
+		Logger: func(r *http.Request, err error) {
+			if err != nil {
+				log.Printf("[WARNING] WebDAV GO Server :: WEBDAV.ERROR: %s [%s %s %s] %s [%s] %s\n", err, r.Method, r.URL, r.Proto, "*", r.Host, r.RemoteAddr)
+			} else {
+				log.Printf("[OK] WebDAV GO Server :: WEBDAV [%s %s %s] %s [%s] %s\n", r.Method, r.URL, r.Proto, "*", r.Host, r.RemoteAddr)
+			}
+		},
+	}
+
+	http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
+		var statusCode = 202
+		if r.URL.Path != "/" {
+			statusCode = 404
+			w.WriteHeader(statusCode)
+			w.Write([]byte("404 Not Found\n"))
+			log.Printf("[ERROR] WebDAV GO Server :: DEFAULT.ERROR [%s %s %s] %s [%s] %s\n", r.Method, r.URL, r.Proto, strconv.Itoa(statusCode), r.Host, r.RemoteAddr)
+			return
+		}
+		log.Printf("[OK] WebDAV GO Server :: DEFAULT [%s %s %s] %s [%s] %s\n", r.Method, r.URL, r.Proto, strconv.Itoa(statusCode), r.Host, r.RemoteAddr)
+		w.Header().Set("Content-Type", smart.HTML_CONTENT_HEADER)
+		w.WriteHeader(statusCode) // status code must be after content type
+		var titleText string = "WebDAV GO Server " + THE_VERSION
+		var headHtml string = "<style>" + "\n" + "div.status { text-align:center; margin:10px; cursor:help; }" + "\n" + "div.signature { background:#778899; color:#FFFFFF; font-size:2rem; font-weight:bold; text-align:center; border-radius:3px; padding:10px; margin:20px; }" + "\n" + "</style>"
+		var bodyHtml string = `<div class="status"><img alt="Status: Up and Running ..." title="Status: Up and Running ..." width="96" height="96" src="data:image/svg+xml,` + smart.EscapeHtml(smart.EscapeUrl(smart.SVG_SPIN)) + `"></div>` + "\n" + `<div class="signature">` + "\n" + "<pre>" + "\n" + smart.EscapeHtml(serverSignature.String()) + "</pre>" + "\n" + "</div>"
+		w.Write([]byte(smart.HtmlSimpleTemplate(titleText, headHtml, bodyHtml)))
+	})
+
+	// http version handler
+	http.HandleFunc("/version", func(w http.ResponseWriter, r *http.Request) {
+		var statusCode = 203
+		log.Printf("[OK] WebDAV GO Server :: VERSION [%s %s %s] %s [%s] %s\n", r.Method, r.URL, r.Proto, strconv.Itoa(statusCode), r.Host, r.RemoteAddr)
+		// plain/text
+		w.WriteHeader(statusCode)
+		w.Write([]byte("WebDAV GO Server " + THE_VERSION + "\n"))
+	})
+
+	// webdav handler
+	http.HandleFunc(DAV_PATH+"/", func(w http.ResponseWriter, r *http.Request) {
+		// test if basic auth
+		user, pass, ok := r.BasicAuth()
+		// check if basic auth and if credentials match
+		if !ok || subtle.ConstantTimeCompare([]byte(user), []byte(authUser)) != 1 || subtle.ConstantTimeCompare([]byte(pass), []byte(authPass)) != 1 {
+			w.Header().Set("WWW-Authenticate", `Basic realm="WebDAV GO Server Storage Area"`)
+			w.WriteHeader(401) // status code must be after set headers
+			w.Write([]byte("401 Unauthorized\n"))
+			log.Printf("[WARNING] WebDAV GO Server :: AUTH.FAILED [%s %s %s] %s [%s] %s\n", r.Method, r.URL, r.Proto, "401", r.Host, r.RemoteAddr)
+			return
+		}
+		// if all ok above (basic auth + credentials ok, serve ...)
+		srv.ServeHTTP(w, r)
+	})
+
+	if serveSecure == true {
+		if(!smart.PathIsFile(TLS_CERT)) {
+			log.Println("[ERROR] INIT TLS: No certificate crt found in current directory. Please provide a valid cert:", TLS_CERT)
+			return false
+		}
+		if(!smart.PathIsFile(TLS_KEY)) {
+			log.Println("[ERROR]: INIT TLS No certificate key found in current directory. Please provide a valid cert:", TLS_KEY)
+			return false
+		}
+		log.Println("[NOTICE] ... serving HTTPS / TLS on port", httpsPort)
+		go http.ListenAndServeTLS(httpAddr + fmt.Sprintf(":%d", httpsPort), TLS_CERT, TLS_KEY, nil)
+	}
+	if disableUnsecure != true {
+		log.Println("[NOTICE] ... serving HTTP on port", httpPort)
+		go http.ListenAndServe(httpAddr + fmt.Sprintf(":%d", httpPort), nil)
+	}
+
+	if disableUnsecure == true && serveSecure != true {
+		log.Println("[ERROR] ... WebDAV NOT Started")
+		return false
+	}
+
+	//--
+	return true
+	//--
+
+} //END FUNCTION
 
 //-----
 

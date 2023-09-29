1dbf23bf18f2b43fca3a1cabddd4b2d631d53954
unix-world <iradu@unix-world.org>
2023-09-29 20:19:03 +0300

    Stable, svn.1061

--- smartgoext.go
+++ smartgoext.go
@@ -1,14 +1,14 @@
 
 // GO Lang :: SmartGo Extra :: Smart.Go.Framework
 // (c) 2020-2023 unix-world.org
-// r.20230926.1746 :: STABLE
+// r.20230929.2004 :: STABLE
 
 // REQUIRE: go 1.17 or later
 package smartgoext
 
 //-----
 
-const VERSION string = "v.20230926.1746"
+const VERSION string = "v.20230929.2004"
 
 //-----
 

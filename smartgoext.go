82b3ac2c28bfd71eecd19f03f81dea5558adda41
Radu Ovidiu I. <ilies.ovidiu@googlemail.com>
2024-09-30 16:26:58 +0300

    SmartGoExt, svn.1755

--- smartgoext.go
+++ smartgoext.go
@@ -1,7 +1,7 @@
 
 // GO Lang :: SmartGo Extra :: Smart.Go.Framework
 // (c) 2020-2024 unix-world.org
-// r.20240928.0102 :: STABLE
+// r.20240930.1531 :: STABLE
 
 // REQUIRE: go 1.19 or later
 package smartgoext
@@ -9,7 +9,7 @@ package smartgoext
 //-----
 
 const (
-	VERSION string = "v.20240928.0102"
+	VERSION string = "v.20240930.1531"
 	NAME string = "SmartGoExt"
 )
 
